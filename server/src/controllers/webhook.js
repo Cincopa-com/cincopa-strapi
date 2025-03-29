557f57ce89b5da4ddfb3e92039ef504122329756
davitCincop <davit@cincopa.com>
2025-03-29 17:10:38 +0000

    handling fullCincopaSync option

--- server/src/controllers/webhook.js
+++ server/src/controllers/webhook.js
@@ -5,15 +5,22 @@ const ENTRY_NAME = `plugin::${PLUGIN_NAME}.${SINGULAR_NAME}`;
 export default {
   async handleWebhook(ctx) {
     try {
+      console.log("FULL SYNC", strapi.config.get(`plugin.${PLUGIN_NAME}.fullCincopaSync`));
       console.log("Webhook received:", ctx.request.body);
       ctx.body = { message: "Webhook received successfully2", data: ctx.request };
 
       const body = ctx?.request?.body;
       const event = body?.event;
-      
+
+      const fullSyncEnabled = strapi.config.get(`plugin.${PLUGIN_NAME}.fullCincopaSync`);
+
       /* we will use asset.update when mirroring not enabled to be able handle reference_id=strapi */
+      if(fullSyncEnabled && event == 'asset.uploaded'){
+        return await handleAssetUpdated(ctx, body, fullSyncEnabled);
+      }
+
       if (event === 'asset.updated') {
-        return await handleAssetUpdated(ctx, body);
+        return await handleAssetUpdated(ctx, body, fullSyncEnabled); 
       }
 
       if (event === 'asset.deleted') {
@@ -25,15 +32,16 @@ export default {
     } catch (error) {
       console.error("Error processing webhook:", error);
       ctx.status = 500;
-      ctx.body = { error: "Internal Server Error" }; 
+      ctx.body = { error: "Internal Server Error" };
     }
   },
 };
 
-async function handleAssetUpdated(ctx, data) {
+
+async function handleAssetUploaded(ctx, data, fullSyncEnabled = false) {
   const reference = data?.reference_id;
   const assetRid = data?.drid;
-
+ 
   // Check if the asset already exists
   const existingAsset = await strapi.db
     .query(ENTRY_NAME)
@@ -42,7 +50,8 @@ async function handleAssetUpdated(ctx, data) {
     });
 
   // reference == 'strapi' MUST BE CHECKED IF ITS NOT FULL MIRRORING OF CINCOPA
-  if (existingAsset && reference === 'strapi') {
+
+  if (existingAsset && (reference === 'strapi' || fullSyncEnabled)) {
     const newAssetData = extractAssetData(data);
 
     const result = await strapi.entityService.update(
@@ -54,7 +63,48 @@ async function handleAssetUpdated(ctx, data) {
     return ctx.send({ success: false, message: 'Asset already exists and updated' });
   }
 
-  if (!existingAsset && reference === 'strapi') {
+  if (!existingAsset && (reference === 'strapi' || fullSyncEnabled)) {
+    const newAssetData = extractAssetData(data);
+
+    const newItem = await strapi.entityService.create(
+      ENTRY_NAME,
+      { data: newAssetData }
+    );
+
+    return ctx.send({ success: true, entry: newItem });
+  }
+
+  return ctx.send({ success: true, message: 'No action taken (non-strapi reference)' });
+}
+
+
+
+async function handleAssetUpdated(ctx, data, fullSyncEnabled = false) {
+  const reference = data?.reference_id;
+  const assetRid = data?.drid;
+ 
+  // Check if the asset already exists
+  const existingAsset = await strapi.db
+    .query(ENTRY_NAME)
+    .findOne({
+      where: { asset_rid: assetRid },
+    });
+
+  // reference == 'strapi' MUST BE CHECKED IF ITS NOT FULL MIRRORING OF CINCOPA
+
+  if (existingAsset && (reference === 'strapi' || fullSyncEnabled)) {
+    const newAssetData = extractAssetData(data);
+
+    const result = await strapi.entityService.update(
+      ENTRY_NAME,
+      existingAsset.id,
+      { data: newAssetData }
+    );
+
+    return ctx.send({ success: false, message: 'Asset already exists and updated' });
+  }
+
+  if (!existingAsset && (reference === 'strapi' || fullSyncEnabled)) {
     const newAssetData = extractAssetData(data);
 
     const newItem = await strapi.entityService.create(
