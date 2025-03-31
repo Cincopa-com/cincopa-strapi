a07852cdb3b7e1a5d43299828b0f924711a22e94
davitCincop <davit@cincopa.com>
2025-03-31 12:13:33 +0000

    Content-Security-Policy fixes in plugin itself

--- server/src/bootstrap.js
+++ server/src/bootstrap.js
@@ -1,5 +1,19 @@
 const bootstrap = ({ strapi }) => {
-  // bootstrap phase
+  const cspMiddleware = async (ctx, next) => {
+    ctx.set('Content-Security-Policy', [
+      "default-src 'self'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' *.cincopa.com fonts.gstatic.com data:",
+      "style-src 'self' 'unsafe-inline' *.cincopa.com fonts.googleapis.com",
+      "img-src 'self' data: blob: *.cincopa.com fonts.gstatic.com",
+      "font-src 'self' fonts.gstatic.com *.cincopa.com  data:", // <- wildcard at end if needed
+      "media-src 'self' blob: *.cincopa.com",
+      "connect-src 'self' *.cincopa.com http: https: ws: data: blob:",
+      "worker-src 'self' blob:",
+    ].join('; '));
+    await next();
+  };
+
+  strapi.server.use(cspMiddleware);
 };
 
 export default bootstrap;
