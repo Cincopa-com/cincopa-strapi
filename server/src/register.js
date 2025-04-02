2b5382d72f3077bf22d5fe5a21ceeaf310b364dd
Arshak <arshakalexanyan@gmail.com>
2025-04-03 02:31:05 +0400

    fix for CSP

--- server/src/register.js
+++ server/src/register.js
@@ -1,6 +1,5 @@
-import permissions from './permissions.js';
 const register = ({ strapi }) => {
-  strapi.container.get('admin.permissions').registerMany(permissions);
+
 };
 
 export default register;
